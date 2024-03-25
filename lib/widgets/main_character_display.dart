d071ed9e46f0d9ced71016263b7c66a234e5de09
woo3145 <lcwoo3145@gmail.com>
2024-03-25 11:19:20 +0900

    Draft main page UI

---
+++ lib/widgets/main_character_display.dart
@@ -0,0 +1,68 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class MainCharacterDisplay extends StatefulWidget {
+  final double width;
+  const MainCharacterDisplay({super.key, required this.width});
+
+  @override
+  State<MainCharacterDisplay> createState() => _MainCharacterDisplayState();
+}
+
+class _MainCharacterDisplayState extends State<MainCharacterDisplay> {
+  String message = "Welcome!";
+  List<String> messages = ["Hello!", "Welcome!", "Enjoy!"];
+  int messageIndex = 0;
+  bool showMessage = true;
+
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(const Duration(seconds: 10), (timer) {
+      setState(() {
+        messageIndex = (messageIndex + 1) % messages.length;
+        message = messages[messageIndex];
+        showMessage = true;
+      });
+      Future.delayed(const Duration(seconds: 3), () {
+        setState(() {
+          showMessage = false;
+        });
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: [
+        Image.network(
+          "https://d2quahb2ygxiv.cloudfront.net/81459a46ef4fdd77fb5ff.png",
+          width: widget.width,
+          fit: BoxFit.contain,
+        ),
+        if (showMessage)
+          Positioned(
+            bottom: 190,
+            left: 0,
+            right: 0,
+            child: Container(
+              color: Colors.white70, // 혹시 이미지랑 겹치면 약간의 배경색으로 보이도록
+              child: Text(
+                message,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+                softWrap: true,
+                overflow: TextOverflow.visible,
+              ),
+            ),
+          ),
+      ],
+    );
+  }
+}

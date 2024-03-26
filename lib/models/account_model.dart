3b7a6f3fbaa90c226983b9e0312436ac2823a059
woo3145 <lcwoo3145@gmail.com>
2024-03-27 07:42:19 +0900

    Add account and member model for auth

---
+++ lib/models/account_model.dart
@@ -0,0 +1,45 @@
+class AccountModel {
+  final String accountId;
+  final String? email;
+  final String? provider;
+  final String? socialId;
+  final Role role;
+  final DateTime createdAt;
+
+  AccountModel({
+    required this.accountId,
+    required this.role,
+    required this.createdAt,
+    this.email,
+    this.provider,
+    this.socialId,
+  });
+
+  factory AccountModel.fromJson(Map<String, dynamic> json) {
+    return AccountModel(
+      accountId: json['account_id'],
+      email: json['email'],
+      provider: json['provider'],
+      socialId: json['social_id'],
+      role: Role.fromJson(json['role']),
+      createdAt: DateTime.parse(json['created_at']),
+    );
+  }
+}
+
+class Role {
+  final int roleId;
+  final String name;
+
+  Role({
+    required this.roleId,
+    required this.name,
+  });
+
+  factory Role.fromJson(Map<String, dynamic> json) {
+    return Role(
+      roleId: json['role_id'],
+      name: json['name'],
+    );
+  }
+}

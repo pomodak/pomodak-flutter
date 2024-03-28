1bd07299ed2704fa006b61f37b1b4de1426bbee4
woo3145 <lcwoo3145@gmail.com>
2024-03-28 23:35:03 +0900

    Add message util for notification

---
+++ lib/utils/message_util.dart
@@ -0,0 +1,30 @@
+import 'package:another_flushbar/flushbar.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class MessageUtil {
+  static toastMessage(String message) {
+    Fluttertoast.showToast(
+      msg: message,
+      backgroundColor: Colors.white,
+      textColor: Colors.black,
+    );
+  }
+
+  static void flushbarErrorMessage(String message, BuildContext context) {
+    Flushbar(
+      margin: const EdgeInsets.all(8),
+      borderRadius: BorderRadius.circular(8),
+      message: message,
+      icon: const Icon(
+        Icons.error,
+        color: Colors.white,
+      ),
+      messageColor: Colors.white,
+      backgroundColor: Colors.red,
+      flushbarStyle: FlushbarStyle.FLOATING,
+      flushbarPosition: FlushbarPosition.TOP,
+      duration: const Duration(seconds: 3),
+    ).show(context);
+  }
+}

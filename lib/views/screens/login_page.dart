f738f7a91d86fda83ba5fd91bfe2317677bede74
woo3145 <lcwoo3145@gmail.com>
2024-03-30 00:00:39 +0900

    Add member info in user profile widget

--- lib/views/screens/login_page.dart
+++ lib/views/screens/login_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:pomodak/router/route_utils.dart';
 import 'package:pomodak/view_models/auth_view_model.dart';
 import 'package:pomodak/views/widgets/button.dart';
--- lib/views/screens/splash_page.dart
+++ lib/views/screens/splash_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:pomodak/router/route_utils.dart';
 import 'package:pomodak/view_models/app_view_model.dart';
 import 'package:pomodak/view_models/auth_view_model.dart';
+import 'package:pomodak/view_models/member_view_model.dart';
 import 'package:provider/provider.dart';
 
 class SplashPage extends StatefulWidget {
@@ -14,19 +15,22 @@ class SplashPage extends StatefulWidget {
 class _SplashPageState extends State<SplashPage> {
   late AppViewModel _appViewModel;
   late AuthViewModel _authViewModel;
+  late MemberViewModel _memberViewModel;
 
   @override
   void initState() {
     // 첫 실행에만 사용되기에 listen: false
     _appViewModel = Provider.of<AppViewModel>(context, listen: false);
     _authViewModel = Provider.of<AuthViewModel>(context, listen: false);
+    _memberViewModel = Provider.of<MemberViewModel>(context, listen: false);
     onStartUp();
     super.initState();
   }
 
   void onStartUp() async {
-    await _authViewModel.onAppStart();
-    await _appViewModel.onAppStart();
+    await _appViewModel.onAppStart(); // 앱 설정 로드
+    await _authViewModel.onAppStart(); // 로그인 정보(토큰, 계정) 로드
+    await _memberViewModel.onAppStart(); // 유저 정보 로드 (토큰 사용)
   }
 
   @override

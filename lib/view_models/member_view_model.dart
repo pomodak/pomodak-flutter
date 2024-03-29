6c813efba67ed3f5f231a87021a6e946aa7145bc
woo3145 <lcwoo3145@gmail.com>
2024-03-29 23:00:41 +0900

    Implement member view model and repository

---
+++ lib/view_models/member_view_model.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:pomodak/models/member_model.dart';
+import 'package:pomodak/repositories/member_repository.dart';
+
+class MemberViewModel with ChangeNotifier {
+  final _myRepo = MemberRepository();
+
+  MemberModel? _member;
+
+  MemberModel? get member => _member;
+
+  Future<MemberModel?> loadMember() async {
+    _member = await _myRepo.fetchMemberData();
+    notifyListeners();
+    return _member;
+  }
+
+  Future<void> remove() async {
+    return _myRepo.clearMemberData();
+  }
+
+  Future<void> onAppStart() async {
+    await loadMember();
+    notifyListeners();
+  }
+}

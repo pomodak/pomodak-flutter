f0aba64f1756bd3ba4873b88e86fb7ff82d9d074
woo3145 <lcwoo3145@gmail.com>
2024-03-30 20:29:19 +0900

    Implement kakao oauth login

--- lib/view_models/auth_view_model.dart
+++ lib/view_models/auth_view_model.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:pomodak/repositories/auth_repository.dart';
 import 'package:pomodak/utils/message_util.dart';
 import 'package:pomodak/models/account_model.dart';
@@ -80,6 +82,31 @@ class AuthViewModel with ChangeNotifier {
     setLoading(false);
   }
 
+  Future<void> kakaoLogin(BuildContext context) async {
+    setLoading(true);
+
+    try {
+      String accessToken = await signInWithKakao();
+      await _myRepo.kakaoLoginApi(accessToken: accessToken);
+
+      await loadAccount(); // 계정 갱신
+
+      if (onLoginSuccess != null) {
+        await onLoginSuccess!();
+      }
+      if (context.mounted) {
+        MessageUtil.flushbarSuccessMessage("로그인 성공", context);
+      }
+    } catch (e) {
+      if (context.mounted) {
+        MessageUtil.flushbarErrorMessage(e.toString(), context);
+      }
+      await logOut();
+    }
+
+    setLoading(false);
+  }
+
   Future<void> loadAccount() async {
     _account = await _myRepo.loadAccount();
     _isLoggedIn = _account != null;
@@ -101,5 +128,39 @@ class AuthViewModel with ChangeNotifier {
   Future<void> onAppStart() async {
     await loadAccount();
     notifyListeners();
+  }
+
+  Future<String> signInWithKakao() async {
+    OAuthToken result;
+    if (await isKakaoTalkInstalled()) {
+      try {
+        result = await UserApi.instance.loginWithKakaoTalk();
+      } catch (error) {
+        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+        if (error is PlatformException && error.code == 'CANCELED') {
+          throw '카카오톡으로 로그인 취소';
+        }
+        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
+        try {
+          result = await UserApi.instance.loginWithKakaoAccount();
+        } catch (error) {
+          if (error is PlatformException && error.code == 'CANCELED') {
+            throw '카카오톡으로 로그인 취소';
+          }
+          throw '카카오계정으로 로그인 실패';
+        }
+      }
+    } else {
+      try {
+        result = await UserApi.instance.loginWithKakaoAccount();
+      } catch (error) {
+        if (error is PlatformException && error.code == 'CANCELED') {
+          throw '카카오톡으로 로그인 취소';
+        }
+        throw '카카오계정으로 로그인 실패';
+      }
+    }
+    return result.accessToken;
   }
 }

53ad573c22dc1e7af984ddce74bfdb76319074a4
woo3145 <lcwoo3145@gmail.com>
2024-04-06 20:05:29 +0900

    Refactor ShopScreen to replace tabController to defaultTabController

--- lib/views/screens/main/shop_screen/shop_screen.dart
+++ lib/views/screens/main/shop_screen/shop_screen.dart
@@ -1,100 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:pomodak/models/domain/item_model.dart';
 import 'package:pomodak/view_models/shop_view_model.dart';
 import 'package:pomodak/views/screens/main/shop_screen/widgets/shop_list_item.dart';
 import 'package:provider/provider.dart';
 
-class ShopScreen extends StatefulWidget {
+class ShopScreen extends StatelessWidget {
   const ShopScreen({super.key});
 
   @override
-  State<ShopScreen> createState() => _ShopScreenState();
-}
-
-class _ShopScreenState extends State<ShopScreen>
-    with SingleTickerProviderStateMixin {
-  late TabController _tabController;
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(length: 2, vsync: this);
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      if (mounted) {
-        final shopViewModel =
-            Provider.of<ShopViewModel>(context, listen: false);
-        shopViewModel.loadShop();
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('상점'),
-        bottom: TabBar(
-          controller: _tabController,
-          tabs: const <Widget>[
-            Tab(
-              child: Text(
-                "알",
-                style: TextStyle(
-                  fontWeight: FontWeight.w600,
-                  letterSpacing: 2,
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Provider.of<ShopViewModel>(context, listen: false).loadShop();
+    });
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('상점'),
+          bottom: const TabBar(
+            tabs: <Widget>[
+              Tab(
+                child: Text(
+                  "알",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: 2,
+                  ),
                 ),
               ),
-            ),
-            Tab(
-              child: Text(
-                "아이템",
-                style: TextStyle(
-                  fontWeight: FontWeight.w600,
-                  letterSpacing: 2,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-      body: Consumer<ShopViewModel>(
-        builder: (context, shop, child) {
-          return TabBarView(
-            controller: _tabController,
-            children: <Widget>[
-              SingleChildScrollView(
-                child: Column(
-                  children: [
-                    for (var item in shop.foodItems)
-                      ShopListItem(
-                        imageUrl: item.imageUrl,
-                        name: item.name,
-                        price: item.cost,
-                        requiredTime: item.requiredStudyTime,
-                      )
-                  ],
-                ),
-              ),
-              SingleChildScrollView(
-                child: Column(
-                  children: [
-                    for (var item in shop.consumableItems)
-                      ShopListItem(
-                        imageUrl: item.imageUrl,
-                        name: item.name,
-                        price: item.cost,
-                      )
-                  ],
+              Tab(
+                child: Text(
+                  "아이템",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: 2,
+                  ),
                 ),
               ),
             ],
-          );
-        },
+          ),
+        ),
+        body: Consumer<ShopViewModel>(
+          builder: (context, shopViewModel, child) {
+            return TabBarView(
+              children: <Widget>[
+                ShopItemsList(items: shopViewModel.foodItems),
+                ShopItemsList(items: shopViewModel.consumableItems),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class ShopItemsList extends StatelessWidget {
+  final List<ItemModel> items;
+
+  const ShopItemsList({super.key, required this.items});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: items
+            .map(
+              (item) => ShopListItem(
+                imageUrl: item.imageUrl,
+                name: item.name,
+                price: item.cost,
+                requiredTime: item.requiredStudyTime,
+              ),
+            )
+            .toList(),
       ),
     );
   }

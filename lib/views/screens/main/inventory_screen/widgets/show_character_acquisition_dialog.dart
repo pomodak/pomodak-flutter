fe9658f98b2e20c00b65ee427a89d0d06a5cf6b3
woo3145 <lcwoo3145@gmail.com>
2024-04-13 10:19:19 +0900

    Add confetti effect after character acquisition

--- lib/views/screens/main/inventory_screen/widgets/show_character_acquisition_dialog.dart
+++ lib/views/screens/main/inventory_screen/widgets/show_character_acquisition_dialog.dart
@@ -1,3 +1,4 @@
+import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
 import 'package:pomodak/models/api/members/consume_item_response.dart';
 import 'package:pomodak/models/domain/character_model.dart';
@@ -17,26 +18,97 @@ void showCharacterAcquisitionDialog(
   );
 }
 
-class CharacterAcquisitionDialog extends StatelessWidget {
+class CharacterAcquisitionDialog extends StatefulWidget {
   final CharacterAcquisition result;
 
   const CharacterAcquisitionDialog({super.key, required this.result});
 
   @override
+  State<CharacterAcquisitionDialog> createState() =>
+      _CharacterAcquisitionDialogState();
+}
+
+class _CharacterAcquisitionDialogState
+    extends State<CharacterAcquisitionDialog> {
+  late ConfettiController confettiController;
+  late int confettiduration;
+  late double emissionFrequency;
+  late double minBlastForce;
+  late double maxBlastForce;
+
+  @override
+  void initState() {
+    super.initState();
+    setConfettiProperties(widget.result.character);
+    confettiController = ConfettiController(
+      duration: Duration(seconds: confettiduration),
+    );
+    confettiController.play();
+  }
+
+  @override
+  void dispose() {
+    confettiController.dispose();
+    super.dispose();
+  }
+
+  void setConfettiProperties(CharacterModel character) {
+    switch (character.grade) {
+      case CharacterGrade.rare:
+        emissionFrequency = 0.02;
+        minBlastForce = 3;
+        maxBlastForce = 3.1;
+        confettiduration = 2;
+        break;
+      case CharacterGrade.epic:
+        emissionFrequency = 0.03;
+        minBlastForce = 6;
+        maxBlastForce = 10;
+        confettiduration = 4;
+        break;
+      case CharacterGrade.legendary:
+        emissionFrequency = 0.04;
+        minBlastForce = 6;
+        maxBlastForce = 20;
+        confettiduration = 8;
+        break;
+      default:
+        emissionFrequency = 0.01;
+        minBlastForce = 1;
+        maxBlastForce = 1.1;
+        confettiduration = 1;
+        break;
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Dialog.fullscreen(
-      backgroundColor: Colors.white,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 56, horizontal: 24),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            _CharacterDetails(character: result.character),
-            _ConfirmButton(),
-          ],
+    return Stack(
+      alignment: Alignment.topCenter,
+      children: [
+        Dialog.fullscreen(
+          backgroundColor: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 56, horizontal: 24),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                _CharacterDetails(character: widget.result.character),
+                _ConfirmButton(),
+              ],
+            ),
+          ),
         ),
-      ),
+        ConfettiWidget(
+          confettiController: confettiController,
+          shouldLoop: false,
+          blastDirectionality: BlastDirectionality.explosive,
+          emissionFrequency: emissionFrequency,
+          minBlastForce: minBlastForce,
+          maxBlastForce: maxBlastForce,
+        )
+      ],
     );
   }
 }
